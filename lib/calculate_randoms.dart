b9ce0832da23e2fcf0c5a01d6ab83b768e1054b6
Andriy Syrokomskyi <signmotion@gmail.com>
2022-02-18 10:47:32 +0200

    Removed a test file.

--- lib/calculate_randoms.dart
+++
@@ -1,6 +0,0 @@
-import 'dart:math';
-
-List<int> calculateRandoms(int seed, [int maxValue = 1000000, int n = 10]) {
-  final rand = Random(seed);
-  return List.generate(n, (index) => rand.nextInt(maxValue));
-}
