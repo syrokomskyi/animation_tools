010991dea4dd93c989ad40187db0abdb3cb1f50f
Andriy Syrokomskyi <signmotion@gmail.com>
2022-02-18 09:39:00 +0200

    Removed unused test.

--- test/calculate_randoms_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:animation_tools/calculate_randoms.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculateRandoms', () {
-    const seed = 12345;
-    expect(calculateRandoms(seed, 50, 3), <int>[31, 48, 30]);
-  });
-}
